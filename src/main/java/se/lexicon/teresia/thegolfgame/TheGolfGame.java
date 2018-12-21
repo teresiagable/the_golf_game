af849ef22f6a5f942b8b7ceee2d29ca0616754cc
Teresia Gable <teresia.gable>
2018-12-21 14:09:57 +0100

    end of work friday

--- src/main/java/se/lexicon/teresia/thegolfgame/TheGolfGame.java
+++ src/main/java/se/lexicon/teresia/thegolfgame/TheGolfGame.java
@@ -12,14 +12,23 @@ public class TheGolfGame {
 	public static final double PI = Math.PI;
 	public static final double minDistance = 100;
 	public static final double maxDistance = 1000;
-	public static final double maxNumberOfHits = 30;
-	public static double[][] courseData = new double[0][4];
 
-	public static Random rand = new Random();
+	// Double.MIN_VALUE is used instead of 0 because I want the min and max values
+	// to be INCLUDED in the range
+	// Double.MIN_VALUE is the closes value to 0 I can get (as far as I know)
+	public static final double minVelocity = Double.MIN_VALUE;
+	public static final double maxVelocity = 151;
+	public static final double minAngle = Double.MIN_VALUE;
+	public static final double maxAngle = 90 - Double.MIN_VALUE;
+
+	public static final double maxNumberOfHits = 15;
+	public static double[][] courseData;
+
+	public static Random rng = new Random();
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		double startingPoint;
+
 		double goalDistance;
 		double givenAngle;
 		double givenVelocity;
@@ -28,21 +37,23 @@ public class TheGolfGame {
 		System.out.println("Welcome to The Golf Game");
 		do {
 			goalDistance = createNewHole(minDistance, maxDistance);
-			while (goalDistance >= 0.1) {
+			while (goalDistance >= 0.1 && courseData.length < maxNumberOfHits) {
 				System.out
-						.println("The cup is " + goalDistance + " m away. Enter the angle and velocity of your stroke");
+						.println("The cup is " + goalDistance + " m away. Enter the angle and velocity of your swing");
 				givenAngle = getInputAngle();
 				givenVelocity = getInputVelocity();
 				hitLength = calculateDistance(givenAngle, givenVelocity);
 				System.out.println("Your ball flew " + Double.toString(hitLength) + " m");
-				;
+
 				saveGamedata(goalDistance, givenAngle, givenVelocity, hitLength);
 				goalDistance = round(Math.abs(goalDistance - hitLength), 2);
 				// System.out.println("The new distance to the cup is " + goalDistance);
 
 			}
-			System.out.println("You hit the hole!");
 			printGameData();
+			if ( goalDistance < 0.1) System.out.println("You hit the hole!");
+			else System.out.println("Leave or try from the beginning!");
+			
 
 		} while (continueOrNot());
 
@@ -51,10 +62,13 @@ public class TheGolfGame {
 	private static void printGameData() {
 		System.out.println("Game History");
 		System.out.println("Distance\tAngle\tVelocity\tLength");
-		for (int i = 0; i <= courseData.length - 1; i++) {
 
-			System.out.println((i + 1) + " : " + courseData[i][0] + "\t" + courseData[i][1] + "\t" + courseData[i][2]
-					+ "\t\t" + courseData[i][3]);
+		int i = 0;
+		for (double[] swingData : courseData) {
+
+			System.out.println(
+					(i + 1) + " : " + swingData[0] + "\t" + swingData[1] + "\t" + swingData[2] + "\t\t" + swingData[3]);
+			i++;
 		}
 		System.out.println("Totally " + courseData.length + " number of hits");
 
@@ -84,6 +98,7 @@ public class TheGolfGame {
 	private static void saveGamedata(double goalDistance2, double givenAngle, double givenVelocity, double hitLength) {
 
 		courseData = Arrays.copyOf(courseData, courseData.length + 1); // add one row for the new stroke
+
 		double[][] hitData = new double[1][4];
 		hitData[0][0] = goalDistance2;
 		hitData[0][1] = givenAngle;
@@ -108,30 +123,46 @@ public class TheGolfGame {
 		String input = scanner.nextLine();
 		try {
 			inputVelocity = Double.parseDouble(input);
+			if (rangeCheck(inputVelocity, minVelocity, maxVelocity))
+				return inputVelocity;
+			else
+				throw new NumberFormatException();
 		} catch (NumberFormatException e) {
-			System.out.println("Not a valid number");
+			System.out.println("Not a valid number or value out of bounds (must be > 0 and <" + maxVelocity + ") ");
 			return getInputVelocity();
 		}
-		return inputVelocity;
+	}
+
+	private static boolean rangeCheck(double theValue, double minValue, double maxValue) {
+		if (theValue >= minValue && theValue <= maxValue)
+			return true;
+		else
+			return false;
 	}
 
 	private static double getInputAngle() {
 		double inputAngle;
+
 		System.out.print("Angle (in degrees): ");
 		String input = scanner.nextLine();
 		try {
 			inputAngle = Double.parseDouble(input);
+			if (rangeCheck(inputAngle, minAngle, maxAngle))
+				return inputAngle;
+			else
+				throw new NumberFormatException();
 		} catch (NumberFormatException e) {
-			System.out.println("Not a valid number");
+			System.out.println("Not a valid number value out of bounds (must be > 0 and < " + maxAngle + ")");
 			return getInputAngle();
 		}
-		return inputAngle;
 	}
 
 	private static double createNewHole(double min, double max) {
 
+		// delete old data if any
+		courseData = new double[0][4];
 		// Randomize length of the course
-		double len = rand.nextDouble() * (max - min) + min;
+		double len = rng.nextDouble() * (max - min) + min;
 		return round(len, 2);
 	}
 
@@ -143,24 +174,6 @@ public class TheGolfGame {
 		bd = bd.setScale(places, RoundingMode.HALF_UP);
 		return bd.doubleValue();
 	}
-
-//	public static String[] addToArray(String[] source, String toAdd) {
-//		boolean isAdded = false;
-//		
-//		for(int i = 0; i<source.length; i++) {
-//			if(source[i] == null) {
-//				source[i] = toAdd;
-//				isAdded = true;
-//				break;
-//			}			
-//		}
-//		
-//		if(isAdded == false) {
-//			source = expandArray(source);
-//			source[source.length-1] = toAdd;
-//		}
-//		return source;		
-//	}
 
 	public static double[][] expandArray(double[][] source) {
 		return Arrays.copyOf(source, source.length + 1);
