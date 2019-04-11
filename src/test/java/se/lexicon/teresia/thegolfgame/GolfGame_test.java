7907b6984a8c7028cd0c6467b91b52e193b41d3d
Teresia Gable <45754965+teresiagable@users.noreply.github.com>
2019-04-11 12:18:21 +0200

    removed duplicate test

--- src/test/java/se/lexicon/teresia/thegolfgame/GolfGame_test.java
+++ src/test/java/se/lexicon/teresia/thegolfgame/GolfGame_test.java
@@ -14,12 +14,5 @@ public class GolfGame_test {
 		assertEquals(320,theDistance,0);	
 		
 	}
-	@Test
-	public void test_un()
-	{
-		double theDistance = TheGolfGame.calculateDistance((double)45,(double) 56);
-		assertEquals(320,theDistance,0);	
-		
-	}
 
 }
